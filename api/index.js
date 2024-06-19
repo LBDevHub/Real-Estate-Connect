574e7f9c9244b9a07431c39f5444229757b46e39
Kojo LB <lbwiththecodes@gmail.com>
2024-06-19 01:10:59 -0500

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from  'dotenv';
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO)
+.then (() => {
+    console.log('connected to MongoDB!');
+})
+.catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+});
 const app = express();
 
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
-}
-);
+});
