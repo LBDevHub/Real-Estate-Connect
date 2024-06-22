0a66e410fb52a18a85ba732971f9100eb259d120
Kojo LB <lbwiththecodes@gmail.com>
2024-06-22 18:47:30 -0500

    Add redux toolkit

--- front-end/src/Pages/SignIn.jsx
+++ front-end/src/Pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, SignInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+ const {loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -18,7 +20,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch((signInStart));
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,20 +28,16 @@ export default function SignIn() {
         },
         body: JSON.stringify(formData),
       });
-
       const data = await res.json();
-      setLoading(false);
-
-      if (!res.ok) {
-        setError(data.message || 'Something went wrong!');
+      console.log(data);
+      if (data.success === false) {
+          dispatch(SignInFailure(data.message));
         return;
       }
-
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError('An unexpected error occurred. Please try again.');
+      dispatch(SignInFailure(error.message));
     }
   };
 
