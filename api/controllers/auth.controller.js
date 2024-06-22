9371a1e696b72edfcd10743cc694eb496dba3b23
Kojo LB <lbwiththecodes@gmail.com>
2024-06-22 15:42:45 -0500

    Create Sign In Api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../modules/user.module.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 const {username, email, phone, password} = req.body;
@@ -16,3 +17,23 @@ try {
     }
   
 };
+
+export const signin = async (req, res, next) => {
+    const {username, email, password} = req.body;
+    try {
+        const validUser = await User.findOne({
+            $or:[{ username:username},{email: email}]
+        });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Invalid password!'));
+        const token = jwt.sign({id: validUser._id },process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+       next(error);
+    }    
+}
