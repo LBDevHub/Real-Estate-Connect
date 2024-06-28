dcdc833dea98715d8575849c8fcf9400b76619cf
Kojo LB <lbwiththecodes@gmail.com>
2024-06-28 06:55:28 -0500

    Create User Update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,39 @@
+import bycrptjs from 'bcryptjs';
+import User from '../modules/user.module.js';
+import { errorHandler } from '../utils/error.js';
+
+
 export const test = (req, res)=> {
     res.json({
         message: 'Hey There, Dont just give up bro!  Life Could be hard sometimes but just hit back harder.'
     });
+};
+
+
+export const updateUser =  async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You  cant update user if not authorized!"))
+        try {
+            if (req.body.password) {
+                req.body.password = bycrptjs.hashSync(req.body.password, 10)
+            }
+            const updateUser = await User.findByIdAndUpdate(req.params.id,{
+                $set:{
+                    username: req.body.username,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                }
+            }, {new:true})
+
+
+            const {password, ...rest} = updateUser._doc
+
+            res.status(200).json(rest)
+
+        } catch (error) {
+            next(error);
+        }
+
+
 };
--- front-end/src/Pages/Profile.jsx
+++ front-end/src/Pages/Profile.jsx
@@ -3,89 +3,84 @@ import { useEffect, useRef, useState } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 
-
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
-  const [filePerc, setfilePerc] = useState(0);
-  const [ fileUploadError, setFileUploadError] = useState(false);
-  const [formData, setformData] = useState({});
-  
-
-
-
-  // firebase storage
-  // allow read;
-  // allow write: if 
-  // request.resource.size < 2 * 1024 * 1024 &&
-  // request.resource.contentType.matches('images/.*')
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
 
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
     }
   }, [file]);
-  
+
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
     const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
-  
+
     uploadTask.on(
       'state_changed',
       (snapshot) => {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setfilePerc(Math.round(progress));
+        setFilePerc(Math.round(progress));
       },
       (error) => {
         setFileUploadError(true);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
-          setformData({ ...formData, avatar: downloadURL })
+          setFormData({ ...formData, avatar: downloadURL })
         );
       }
     );
   };
-  
       
   return (
-    <div className=' p-3  max-w-lg mx-auto '>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col  gap-4'>
-        <input onChange={(e) => setFile(e.target.files[0])} 
-        type='file' ref={fileRef} hidden accept='image/*' />
-        <img onClick={() =>fileRef.current.click()} src=
-        {formData.avatar || currentUser.avatar} alt="profile"
-          className='rounded-full h-24 w-24 object-cover
-           cursor-pointer  self-center mt-2' />
+      <form className='flex flex-col gap-4'>
+        <input 
+          onChange={(e) => setFile(e.target.files[0])} 
+          type='file' 
+          ref={fileRef} 
+          hidden 
+          accept='image/*' 
+        />
+        <img 
+          onClick={() => fileRef.current.click()} 
+          src={formData.avatar || currentUser.avatar} 
+          alt="profile"
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
+        />
 
-    <p className='self-center text-sm'>
-            {fileUploadError ? (
-             <span className='text-red-700'>Error Image Upload(image must be less than 2 mb)</span>
-              ) : filePerc > 0 && filePerc < 100 ? (
+        <p className='self-center text-sm'>
+          {fileUploadError ? (
+            <span className='text-red-700'>Error Image Upload (image must be less than 2 MB)</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
             <span className='text-slate-700'>
-            {`Uploading ${filePerc}%`}
-      </span>
-           ) : filePerc === 100 ? (
-          <span className='text-green-700'>Image Successfully Uploaded!</span>
-         ) : (
-        ""
-    )}
-</p>
+              {`Uploading ${filePerc}%`}
+            </span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image Successfully Uploaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
 
-        <input type='text' placeholder='Username' id='username'  className='border p-3 rounded-lg' />
-        <input type='email' placeholder='Email' id='email'  className='border p-3 rounded-lg' />
-        <input type='password' placeholder='Password' id='password'  className='border p-3 rounded-lg' />
-        <button className='bg-slate-800 text-white rounded-lg p-3 uppercase hover:bg-[#333]'>update profile</button>
+        <input type='text' placeholder='Username' id='username' className='border p-3 rounded-lg' />
+        <input type='email' placeholder='Email' id='email' className='border p-3 rounded-lg' />
+        <input type='password' placeholder='Password' id='password' className='border p-3 rounded-lg' />
+        <button className='bg-slate-800 text-white rounded-lg p-3 uppercase hover:bg-[#333]'>Update Profile</button>
       </form>
       <div className="flex justify-between mt-5 font-bold">
-          <button className="text-red-700 cursor-pointer bg-transparent border border-red-400 rounded-md py-2 px-4 hover:bg-red-700 hover:text-white transition duration-300">Delete Account</button>
-          <button className="text-red-700 cursor-pointer bg-transparent border border-red-400 rounded-md py-2 px-4 hover:bg-red-700 hover:text-white transition duration-300">Sign Out</button>
-    </div>
-
+        <button className="text-red-700 cursor-pointer bg-transparent border border-red-400 rounded-md py-2 px-4 hover:bg-red-700 hover:text-white transition duration-300">Delete Account</button>
+        <button className="text-red-700 cursor-pointer bg-transparent border border-red-400 rounded-md py-2 px-4 hover:bg-red-700 hover:text-white transition duration-300">Sign Out</button>
+      </div>
     </div>
   );
 }
